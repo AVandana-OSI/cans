fe569d1c6a18670a2a1067005b33c1b9242164a6
Gustavo Gomez <ggomez@cambriasolutions.com>
2018-10-24 17:36:44 -0700

    Remove id from SideNavLink component; Fix SideNavLink test;

--- app/javascript/Application/components/Layout/SideNavLink.test.js
+++ app/javascript/Application/components/Layout/SideNavLink.test.js
@@ -14,7 +14,7 @@ describe('active <SideNavLink />', () => {
   it('renders with active css class', () => {
     expect(
       getWrapper(true)
-        .find('#side-nav')
+        .find('.sidebar-item')
         .hasClass('active')
     ).toBe(true)
   })
@@ -22,7 +22,7 @@ describe('active <SideNavLink />', () => {
   it('renders with no active css class', () => {
     expect(
       getWrapper(false)
-        .find('#side-nav')
+        .find('.sidebar-item')
         .hasClass('active')
     ).toBe(false)
   })
@@ -35,8 +35,8 @@ describe('<SideNavLink /> text', () => {
         <SideNavLink isActive={true} text={'Reports'} href={'/reports'} onClick={jest.fn()} />
       </MemoryRouter>
     )
-    expect(wrapper.find('#side-nav').text()).toBe('Reports')
-    expect(wrapper.find('#side-nav').text()).not.toBe('Report')
+    expect(wrapper.find('.sidebar-item').text()).toBe('Reports')
+    expect(wrapper.find('.sidebar-item').text()).not.toBe('Report')
   })
 })
 
