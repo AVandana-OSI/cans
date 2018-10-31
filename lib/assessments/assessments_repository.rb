cf46b3c7e24773e909848644c071f0c135aae49a
Gustavo Gomez <ggomez@cambriasolutions.com>
2018-10-31 02:03:04 -0700

    Fix code climate error;

--- lib/assessments/assessments_repository.rb
+++ lib/assessments/assessments_repository.rb
@@ -23,7 +23,7 @@ module Assessments
       @http_service.call("/assessments/#{id}", :put, @token, payload)
     end
 
-    def getall()
+    def getall
       @http_service.call('/staff/assessments', :get, @token)
     end
   end
