7ae5c213d020a3463d82115e557a56591ef477d8
money <manmohit.singh@gmail.com>
2018-11-01 13:42:11 -0700

    Update search_params and dora gem

--- app/controllers/api/people_searches_controller.rb
+++ app/controllers/api/people_searches_controller.rb
@@ -1,18 +1,19 @@
 # frozen_string_literal: true
 
 module Api
-    class PeopleSearchesController < ActionController::API
-      def index
-        response = Dora::PersonSearchRepository
-                    .search(search_params,
-                    request.uuid,
-                    security_token: session[:security_token])
-        render json: response
-      end
-
-      private
-      def
-        params.permit(:search_term)
-      end
+  class PeopleSearchesController < ActionController::API
+    def index
+      response = Dora::PersonSearchRepository
+                 .search(search_params,
+                         request.uuid,
+                         security_token: session[:security_token])
+      render json: response
     end
+
+    private
+
+    def search_params
+      params.permit(:search_term, :is_client_only, :search_after)
+    end
+  end
   end
